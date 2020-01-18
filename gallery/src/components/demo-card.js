6462f1b044c4ce5a2c7961f9e9a08780c949733c
Restyled.io <commits@restyled.io>
2020-01-18 01:03:39 +0000

    Restyled by clang-format

--- gallery/src/components/demo-card.js
+++ gallery/src/components/demo-card.js
@@ -1,8 +1,10 @@
-import { html } from "@polymer/polymer/lib/utils/html-tag";
-import { PolymerElement } from "@polymer/polymer/polymer-element";
-import { safeLoad } from "js-yaml";
+import {html} from "@polymer/polymer/lib/utils/html-tag";
+import {PolymerElement} from "@polymer/polymer/polymer-element";
+import {safeLoad} from "js-yaml";
 
-import { createCardElement } from "../../../src/panels/lovelace/create-element/create-card-element";
+import {
+  createCardElement
+} from "../../../src/panels/lovelace/create-element/create-card-element";
 
 class DemoCard extends PolymerElement {
   static get template() {
@@ -44,15 +46,15 @@ class DemoCard extends PolymerElement {
 
   static get properties() {
     return {
-      hass: {
-        type: Object,
-        observer: "_hassChanged",
+      hass : {
+        type : Object,
+        observer : "_hassChanged",
       },
-      config: {
-        type: Object,
-        observer: "_configChanged",
+      config : {
+        type : Object,
+        observer : "_configChanged",
       },
-      showConfig: Boolean,
+      showConfig : Boolean,
     };
   }
 
@@ -69,12 +71,11 @@ class DemoCard extends PolymerElement {
 
   _hassChanged(hass) {
     const card = this.$.card.lastChild;
-    if (card) card.hass = hass;
+    if (card)
+      card.hass = hass;
   }
 
-  _trim(config) {
-    return config.trim();
-  }
+  _trim(config) { return config.trim(); }
 }
 
 customElements.define("demo-card", DemoCard);
