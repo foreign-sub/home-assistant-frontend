926597dbaf7b6cea5476095fd24b8c75b6a5c6d1
Restyled.io <commits@restyled.io>
2020-03-13 17:45:41 +0000

    Restyled by clang-format

--- build-scripts/env.js
+++ build-scripts/env.js
@@ -1,14 +1,6 @@
 module.exports = {
-  isProdBuild() {
-    return process.env.NODE_ENV === "production";
-  },
-  isStatsBuild() {
-    return process.env.STATS === "1";
-  },
-  isTravis() {
-    return process.env.TRAVIS === "true";
-  },
-  isNetlify() {
-    return process.env.NETLIFY === "true";
-  },
+  isProdBuild() { return process.env.NODE_ENV === "production"; },
+  isStatsBuild() { return process.env.STATS === "1"; },
+  isTravis() { return process.env.TRAVIS === "true"; },
+  isNetlify() { return process.env.NETLIFY === "true"; },
 };
--- build-scripts/gulp/app.js
+++ build-scripts/gulp/app.js
@@ -12,41 +12,24 @@ require("./webpack.js");
 require("./service-worker.js");
 require("./entry-html.js");
 
-gulp.task(
-  "develop-app",
-  gulp.series(
-    async function setEnv() {
-      process.env.NODE_ENV = "development";
-    },
-    "clean",
-    gulp.parallel(
-      "gen-service-worker-dev",
-      gulp.parallel("gen-icons-app", "gen-icons-mdi"),
-      "gen-pages-dev",
-      "gen-index-app-dev",
-      "build-translations"
-    ),
-    "copy-static",
-    "webpack-watch-app"
-  )
-);
+gulp.task("develop-app",
+          gulp.series(
+              async function setEnv() { process.env.NODE_ENV = "development"; },
+              "clean",
+              gulp.parallel("gen-service-worker-dev",
+                            gulp.parallel("gen-icons-app", "gen-icons-mdi"),
+                            "gen-pages-dev", "gen-index-app-dev",
+                            "build-translations"),
+              "copy-static", "webpack-watch-app"));
 
 gulp.task(
-  "build-app",
-  gulp.series(
-    async function setEnv() {
-      process.env.NODE_ENV = "production";
-    },
-    "clean",
-    gulp.parallel("gen-icons-app", "gen-icons-mdi", "build-translations"),
-    "copy-static",
-    "webpack-prod-app",
-    ...// Don't compress running tests
-    (envVars.isTravis() ? [] : ["compress-app"]),
-    gulp.parallel(
-      "gen-pages-prod",
-      "gen-index-app-prod",
-      "gen-service-worker-prod"
-    )
-  )
-);
+    "build-app",
+    gulp.series(
+        async function setEnv() { process.env.NODE_ENV = "production"; },
+        "clean",
+        gulp.parallel("gen-icons-app", "gen-icons-mdi", "build-translations"),
+        "copy-static", "webpack-prod-app",
+        ... // Don't compress running tests
+        (envVars.isTravis() ? [] : [ "compress-app" ]),
+        gulp.parallel("gen-pages-prod", "gen-index-app-prod",
+                      "gen-service-worker-prod")));
